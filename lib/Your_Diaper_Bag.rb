e30ed269a13f949b95dd225fbff6327ae036c2e4
RL2020 <loc.fitness1@gmail.com>
2021-01-15 12:19:01 -0500

    set up file requires, dependencies, some gems

---
+++ lib/Your_Diaper_Bag.rb
@@ -0,0 +1,6 @@
+require "Your_Diaper_Bag/version"
+
+module YourDiaperBag
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/Your_Diaper_Bag_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe YourDiaperBag do
+  it "has a version number" do
+    expect(YourDiaperBag::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
